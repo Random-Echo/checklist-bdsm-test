f9b2eb13151c120754bab9e73562d4fe3a99af4b
Random-Echo <adresseunpeusecrete@outlook.fr>
2026-08-19 00:03:42 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ let runtimeProfileCache = null;
 function runtimeProfile(){ return runtimeProfileCache || (runtimeProfileCache = PROFILE_API?.get?.() || {}); }
 const CATALOG_ENTITIES = UNIFIED_CATALOG.entities || [];
 const categoryColors = CHECKLIST_DATA.categoryColors;
-const APP_VERSION = "V1.1.143";
+const APP_VERSION = "V1.1.144";
 const UNIFIED_ENTITY_BY_ID = new Map(CATALOG_ENTITIES.map(entity => [entity.id, entity]));
 
 const LANG_KEY = window.CHECKLIST_SITE.languageKey;
